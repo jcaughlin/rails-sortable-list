b24ac4b9b9b1949d1ed706502bebd99c33d786e7
JS Caughlin <jcaughlin@gmail.com>
2021-08-26 16:54:03 -0500

    Seeing why Stimulusjs is causing issues.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,16 +2,21 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
 import "bootstrap"
 import "stylesheets/application"
 
-window.Rails = Rails;x
+
+// const application = Application.start()
+// const context = require.context("./controllers", true, /\.js$/)
+//application.load(definitionsFromContext(context))
+
+window.Rails = Rails;
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
